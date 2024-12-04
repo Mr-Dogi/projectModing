6ed9f4bdc81c242591097f7a5ba92ac360cfb07c
Mr-Dogi <asd561298@gmail.com>
2024-12-04 09:43:24 +0000

    데이터 베이스 설정 변경

--- ts/src/config/databases.ts
+++ ts/src/config/databases.ts
@@ -2,11 +2,11 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-    host: "34.97.5.240",
+    host: "34.97.91.156",
     port: 3306,
     user: "root",
     password: "R]>m3>uU:_`gpc,a",
-    database: process.env.DB_NAME,
+    database: "projectModing",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
